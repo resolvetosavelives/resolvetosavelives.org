725e4c1d4efbff1691be2b1900e444d630ac8d19
dburka <daniel@gv.com>
2018-07-06 02:03:31 -0400

    Site updated to 60a547d

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+document.addEventListener("DOMContentLoaded", function() {
+    var menuItem = document.querySelector('.menu-item a');
+    console.log(menuItem);
+
+    menuItem.addEventListener('click', function() {
+        var submenu = document.querySelector('.submenu');
+        submenu.classList.toggle('active');
+    });
+});
